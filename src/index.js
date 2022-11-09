8ad89c205f3a9398544f9cab55fd0cd8a5f58265
rnuravtsev <muravtsev21@gmail.com>
2022-11-09 09:01:32 +0300

    some layout

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import {sum} from './modules/sum';
+// import {sum} from './modules/sum';
 // import './scss/main.scss'
 
-const root = document.querySelector('#root');
-root.textContent = sum(6, 434).toString();
+// const root = document.querySelector('#root');
+// root.textContent = sum(6, 434).toString();
