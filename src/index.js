4af9b570441d7e304397ff8a3b1896b96a519495
rnuravtsev <muravtsev21@gmail.com>
2022-11-09 13:53:40 +0300

    add input customization

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-// import {sum} from './modules/sum';
-// import './scss/main.scss'
+const inputs = document.querySelectorAll('.base-input__input')
 
-// const root = document.querySelector('#root');
-// root.textContent = sum(6, 434).toString();
+const toggleLabelClass = (input, label, className = 'base-input__label_shrink') => {
+    if (input.value || document.activeElement === input) {
+        label.classList.add(className)
+    } else {
+        label.classList.remove(className)
+    }
+}
+
+const shrinkInputLabel = ({ target }) => {
+    const label = target.parentElement
+    toggleLabelClass(target, label)
+}
+
+inputs.forEach((el) => el.addEventListener('input', shrinkInputLabel))
+inputs.forEach((el) => el.addEventListener('blur', shrinkInputLabel))
+inputs.forEach((el) => el.addEventListener('focus', shrinkInputLabel))
